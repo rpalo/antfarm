d0aa4562f3a0abe2cf4d7eddbcf8b6d8a6b0c97e
Ryan Palo <ryan@thepalos.com>
2017-06-02 21:12:23 -0700

    Create initial food

---
+++ js/food.js
@@ -0,0 +1,27 @@
+'use strict';
+
+class Foodpile {
+
+    constructor() {
+        this.size = 20;
+        this.position = createVector(random(width), random(height));
+    }
+
+    take(amount) {
+        if (this.size - amount < 0) {
+            amount = this.size;
+        }
+        this.size -= amount;
+        return amount;
+    }
+
+    show() {
+        push();
+        stroke("black");
+        fill("yellow");
+        translate(this.position.x, this.position.y);
+        ellipse(0, 0, this.size, this.size);
+    }
+
+
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
-let a;
+let a, f1, f2;
 
 function setup() {
   createCanvas(600, 400);
   background(255);
   angleMode(DEGREES);
   a = new ForagerAnt();
+  f1 = new Foodpile();
+  f2 = new Foodpile();
 }
 
 function draw() {
   background(255);
   a.update();
   a.show();
+  f1.show();
+  f2.show();
 }
 
